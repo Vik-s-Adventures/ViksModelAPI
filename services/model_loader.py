3ece88cd945ec4cb5e46bf57e5c8247266a9d319
Shayla Choque <u20201c144@upc.edu.pe>
2025-05-16 03:06:29 -0500

    add .gitignore

--- services/model_loader.py
+++ services/model_loader.py
@@ -2,13 +2,16 @@ import os
 import pandas as pd
 from tensorflow.keras.models import load_model
 
-# Ruta absoluta a la carpeta `resources/`
-RUTA_RECURSOS = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "resources"))
+# Ruta absoluta desde app.py a la carpeta resources
+RUTA_RECURSOS = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "resources")
+RUTA_RECURSOS = os.path.abspath(RUTA_RECURSOS) 
 
 def cargar_modelos():
     modelos = {}
     for i in range(1, 5):
         ruta = os.path.join(RUTA_RECURSOS, f"modelo_C{i}.keras")
+        if not os.path.exists(ruta):
+            raise FileNotFoundError(f"Modelo no encontrado: {ruta}")
         modelo = load_model(ruta, compile=False)
         modelo.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
         modelos[f"C{i}"] = modelo
@@ -18,6 +21,8 @@ def cargar_diccionarios():
     diccionarios = {}
     for i in range(1, 5):
         ruta_csv = os.path.join(RUTA_RECURSOS, f"diccionario_rutas_C{i}.csv")
+        if not os.path.exists(ruta_csv):
+            raise FileNotFoundError(f"Diccionario no encontrado: {ruta_csv}")
         diccionario = pd.read_csv(ruta_csv)
         diccionario["ruta"] = diccionario["ruta"].apply(eval)
         diccionarios[f"C{i}"] = diccionario
